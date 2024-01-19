a0850246cf1d56b55805126d9f7fc141b4d64036
MateusLucasDaSilva <mateuslukas505@gmail.com>
2024-01-19 12:05:16 -0300

    criei o projeto

---
+++ android/app/src/main/kotlin/com/example/food_products/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_products
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:food_products/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
